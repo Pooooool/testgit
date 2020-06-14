b19739407ab9f5f0cf64d15ae885509887cad3b3
w1033 <w1033@LAPTOP-7DOUIA71>
2020-06-14 21:55:58 +0800

    test from desktop-2

--- src/testgit/tset.java
+++ src/testgit/tset.java
@@ -4,6 +4,9 @@ public class tset {
 	public static void main(String[] args) {
 		System.out.println("hellogit");
 	}
+	public void desktop() {
+		System.out.println("desktop");
+	}
 }	
 
-//test from desktop
+//test from desktop-2
