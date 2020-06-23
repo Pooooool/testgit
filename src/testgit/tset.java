9d211c4cadd437ccebad28d37c3091da2cc6b496
deadfish <1033581334@qq.com>
2020-06-23 18:22:55 +0800

    18点22分

--- src/testgit/tset.java
+++ src/testgit/tset.java
@@ -5,10 +5,13 @@ public class tset {
 		System.out.println("hellogit");
 		System.out.println("develop");
 		System.out.println("18点17分");
+		System.out.println("18点22分");
 	}
 	public void desktop() {
 		System.out.println("21点57分");
 	}
+	
+	
 }	
 
 //test from pc-18点19分
